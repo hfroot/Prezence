d2fc56b5360e9cb23ba84e2690e585d9a0c85ab5
Helen <helenroot1@gmail.com>
2016-11-15 22:35:15 +0000

    Added ability to check for thresholds as determined by config file

--- central_proc/serial.py
+++ central_proc/serial.py
@@ -1,26 +1,57 @@
 import os
 import time
 
-outputsTxt = open(os.path.dirname(os.path.realpath(__file__))+'/output_filenames.txt', 'r')
+# output data structure:
+# name of output (eg speech_speed), file, check type (eg "min", "inRange", "dict"?), data
+# outputs = {
+#     "speech_speed": {
+#         "file": opened file from files txt,
+#         "checkType": from config,
+#         "data": from config
+#     },
+#     "speech_accuracy": {...}
+# }
 
-outputFiles = {}
+# set up outputs dictionary
+outputs = {}
 outputDir = 'dummy_programs/output/'
-for outputFileName in outputsTxt:
-    name = outputFileName.rstrip('\n')
-    outputFiles[name] = open(outputDir+name+'.txt', 'r')
+configFile = open(os.path.dirname(os.path.realpath(__file__))+'/config.txt', 'r')
+for config in configFile:
+    config = config.split()
+    name = config[0]
+    outputs[name] = { "file": open(outputDir+name+'.txt', 'r') }
+    if len(config) > 1:
+        outputs[name]["checkType"] = config[1]
+        if config[1] == "inRange":
+            outputs[name]["data"] = [config[2], config[3]]
+        else:
+            outputs[name]["data"] = config[2]
+configFile.close()
 
+# read and check outputs
 i = 0
 while i < 10: # safety to stop loop
-    for name, f in outputFiles.iteritems():
+    for name, info in outputs.iteritems():
         # the following inspired by the soln: http://code.activestate.com/recipes/157035-tail-f-in-python/
-        # f = outputFiles["speech_accuracy"]
+        f = info["file"];
         where = f.tell()
         line = f.readline()
-        if not line:
+        if not line or line in ['\n', '\r\n']:
             time.sleep(1)
             f.seek(where)
         else:
             print name+': '+str(line), # already has newline
+            if "checkType" in info:
+                checkType = info["checkType"]
+                if checkType == "min":
+                    if float(line.rstrip('\n')) < info["data"]:
+                        print name+" is too low!"
+                elif checkType == "inRange":
+                    if float(line.rstrip('\n')) < info["data"][0] or float(line.rstrip('\n')) > info["data"][1]:
+                        print name+" is out of range!"
     i+=1
+
+for name, info in outputs.iteritems():
+    info["file"].close()
 
 print "Finished loop"
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 
 # run by: ./scripts/start.sh dummy_programs NAME_OF_CONTROL
 # run programs in parallel
-python central_proc/$2.py & python $1/speech_speed.py & python $1/speech_accuracy.py & python $1/cv_gesture.py
+python central_proc/$2.py & python $1/speech_speed.py & python $1/speech_accuracy.py & python $1/cv_gesture.py & wait
