48c445ccde1239bc831cb06aef4b1c70c8f80a2a
Helen <helenroot1@gmail.com>
2016-11-27 13:05:36 +0000

    Added proper stopping and timeout

--- central_proc/serial.py
+++ central_proc/serial.py
@@ -1,5 +1,7 @@
 import os
 import time
+from interruptingcow import timeout
+TIMEOUT = 60
 
 # output data structure:
 # name of output (eg speech_speed), file, check type (eg "min", "inRange", "dict"?), data
@@ -12,25 +14,7 @@ import time
 #     "speech_accuracy": {...}
 # }
 
-# set up outputs dictionary
-outputs = {}
-outputDir = 'dummy_programs/output/'
-configFile = open(os.path.dirname(os.path.realpath(__file__))+'/config.txt', 'r')
-for config in configFile:
-    config = config.split()
-    name = config[0]
-    outputs[name] = { "file": open(outputDir+name+'.txt', 'r') }
-    if len(config) > 1:
-        outputs[name]["checkType"] = config[1]
-        if config[1] == "inRange":
-            outputs[name]["data"] = [config[2], config[3]]
-        else:
-            outputs[name]["data"] = config[2]
-configFile.close()
-
-# read and check outputs
-i = 0
-while i < 10: # safety to stop loop
+def readFiles(outputs):
     for name, info in outputs.iteritems():
         # the following inspired by the soln: http://code.activestate.com/recipes/157035-tail-f-in-python/
         f = info["file"];
@@ -41,6 +25,8 @@ while i < 10: # safety to stop loop
             f.seek(where)
         else:
             print name+': '+str(line), # already has newline
+            if line.rstrip('\n') == "stop":
+                return 1
             if "checkType" in info:
                 checkType = info["checkType"]
                 if checkType == "min":
@@ -49,9 +35,39 @@ while i < 10: # safety to stop loop
                 elif checkType == "inRange":
                     if float(line.rstrip('\n')) < info["data"][0] or float(line.rstrip('\n')) > info["data"][1]:
                         print name+" is out of range!"
-    i+=1
+    return 0
 
-for name, info in outputs.iteritems():
-    info["file"].close()
+def main():
+    # set up outputs dictionary
+    outputs = {}
+    outputDir = 'dummy_programs/output/'
+    configFile = open(os.path.dirname(os.path.realpath(__file__))+'/config.txt', 'r')
+    for config in configFile:
+        config = config.split()
+        name = config[0]
+        outputs[name] = { "file": open(outputDir+name+'.txt', 'r') }
+        if len(config) > 1:
+            outputs[name]["checkType"] = config[1]
+            if config[1] == "inRange":
+                outputs[name]["data"] = [config[2], config[3]]
+            else:
+                outputs[name]["data"] = config[2]
+    configFile.close()
 
-print "Finished loop"
+    # read and check outputs
+    # using solution given by andresfp to add a timeout: http://stackoverflow.com/a/13293360/3845770
+    try:
+        with timeout(TIMEOUT, exception=RuntimeError):
+            stop = 0
+            while not stop: # safety to stop loop
+                stop = readFiles(outputs)
+            print "Stopping: Module called for stop"
+    except RuntimeError:
+        print "Stopping: timeout (greater than "+str(float(TIMEOUT/60))+" minutes)"
+        pass
+
+    for name, info in outputs.iteritems():
+        info["file"].close()
+    return 0
+
+main()
--- dummy_programs/cv_gesture.py
+++ dummy_programs/cv_gesture.py
@@ -2,11 +2,15 @@ import random
 import os
 import time
 
-gestures = ["shifting weight", "moving", "waving"]
+gestures = ["shifting weight", "moving", "waving", "stop"]
 
 f = open(os.path.dirname(os.path.realpath(__file__))+'/output/cv_gesture.txt', 'w')
-for i in range(5):
-	f.write(gestures[random.randrange(0,3)]+"\n")
+stop = False
+while 1:
+	gesture = gestures[random.randrange(0,4)]
+	f.write(gesture+"\n")
+	if gesture == "stop":
+		break
 	time.sleep(1)
 
 f.close()
