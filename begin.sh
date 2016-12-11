9bb93d59c418a7f9f402db49cae7b6c22bd4060a
Daryl <dm2913@ic.ac.uk>
2016-12-11 15:24:05 +0000

    finalised feedback files with gangnam style

---
+++ begin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# python central_proc.py 
+
+# gnome-terminal
+
+# gnome-terminal -e python | speech/speech.py
+
+x-terminal-emulator -e python speech/speech.py
+
+# roslaunch skeleton_markers skeleton_launch
+# rosrun skeletom_monitor skeleton_monitor
+
+
+# gnome-terminal
+# python speech/speech.py
+
+# gnome-terminal
+# python kinetic_feedback/kinetic_feedback.py
