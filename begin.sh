c58ccba26ff54baeaf9e12754de0f0fdf48c7a68
majpoynton <max.poynton13@imperial.ac.uk>
2016-12-11 16:11:50 +0000

    latest push

--- begin.sh
+++ begin.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+export PYTHONPATH=$PYTHONPATH:/home/human/Prezence/kinetic_feedback/pynaoqi-python2.7-2.1.2.17-linux64
+
 xterm -title "Central Proc" -hold -e python central_proc/tailored_cp.py  &
 xterm -title "Speech Module" -hold -e python speech/speech.py  &
-xterm -title "Computer Vision" -hold -e roslaunch skeleton_markers skeleton_launch | rosrun skeletom_monitor skeleton_monitor  &
-xterm -title "Gesture" -hold -e python kinetic_feedback/kinetic_feedback.py
+xterm -title "Computer Vision" -hold -e roslaunch skeleton_markers skeleton.launch | rosrun skeleton_monitor skeleton_monitor  &
+xterm -title "Gesture" -hold -e  python kinetic_feedback/kinetic_feedback.py
